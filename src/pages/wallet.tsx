508ec570fa46933bd0daf4703875f53bd9c74288
zS1L3NT Mac <zechariahtan144@gmail.com>
2023-05-29 12:17:23 +0800

    ✨ make wallet page mobile responsive

--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -1,7 +1,7 @@
 import { AnimatePresence, motion } from "framer-motion"
 import Head from "next/head"
 import { useRouter } from "next/router"
-import { useMemo, useState } from "react"
+import { useContext, useMemo, useState } from "react"
 
 import { FidorInternalTransfer } from "@/@types/fidor"
 import { SessionUser } from "@/@types/iron-session"
@@ -11,54 +11,30 @@ import {
 } from "@/api/transactions"
 import Shell from "@/components/Shell"
 import { CURRENCIES, CURRENCY, CURRENCY_FLAGS } from "@/constants"
+import UserContext from "@/contexts/UserContext"
 import withSession from "@/utils/withSession"
 import {
-	Badge, Button, Card, Divider, Flex, Grid, NumberInput, Stack, Table, Text, Title,
-	useMantineTheme
+	ActionIcon, Badge, Box, Button, Card, Divider, Drawer, Flex, Grid, NumberInput, Stack, Table,
+	Text, Title, useMantineTheme
 } from "@mantine/core"
+import { useDisclosure, useMediaQuery } from "@mantine/hooks"
 import { notifications } from "@mantine/notifications"
 import { Transaction, TransactionType } from "@prisma/client"
-import { IconCheck } from "@tabler/icons-react"
+import { IconCheck, IconShoppingCart } from "@tabler/icons-react"
 
 type Props = {
 	user: SessionUser
 }
 
-export default function Wallet({ user }: Props) {
-	const theme = useMantineTheme()
+function FidorToMarkex({ isDrawer = false }: { isDrawer?: boolean }) {
 	const router = useRouter()
+	const { user } = useContext(UserContext)
 
 	const [createAppTransaction] = useCreateAppTransactionMutation()
 	const [createFidorInternalTransfer] = useCreateFidorInternalTransferMutation()
-	const { data: transfers } = useGetFidorInternalTransfersQuery()
 
 	const [amount, setAmount] = useState<number | "">(0)
 	const [isLoading, setIsLoading] = useState(false)
-	const transactions: {
-		id: string
-		date: Date
-		app: Transaction | null
-		fidor: FidorInternalTransfer | null
-	}[] = useMemo(
-		() =>
-			[
-				...user.app.transactions.map(t => ({
-					id: t.id,
-					date: new Date(t.created_at),
-					app: t,
-					fidor: transfers?.data.find(tf => tf.external_uid === t.id) ?? null
-				})),
-				...(transfers?.data ?? [])
-					.filter(t => !user.app.transactions.find(tx => tx.id === t.external_uid))
-					.map(t => ({
-						id: t.id!,
-						date: new Date(t.created_at!),
-						app: null,
-						fidor: t
-					}))
-			].sort((a, b) => b.date.getTime() - a.date.getTime()),
-		[user, transfers]
-	)
 
 	const handleTransaction = async () => {
 		if (amount) {
@@ -67,7 +43,7 @@ export default function Wallet({ user }: Props) {
 			const uuid = URL.createObjectURL(new Blob([])).split("/").at(-1)! // UUID hack
 			const fidorResult = await createFidorInternalTransfer({
 				external_uid: uuid,
-				account_id: user.fidor.id!,
+				account_id: user!.fidor.id!,
 				amount,
 				receiver: "11874237",
 				currency: "SGD"
@@ -97,6 +73,73 @@ export default function Wallet({ user }: Props) {
 		}
 	}
 
+	return (
+		<Stack w={isDrawer ? "100%" : 250}>
+			<Title
+				mt="md"
+				order={2}>
+				Fidor to Markex
+			</Title>
+
+			<Text
+				c="dimmed"
+				fz="xs">
+				Transfer SGD from your Fidor Bank Account to your Markex Wallet!
+			</Text>
+
+			<NumberInput
+				label="Amount"
+				description="Amount of SGD to transfer"
+				value={amount}
+				onChange={setAmount}
+				hideControls
+			/>
+
+			<Button
+				color="green"
+				onClick={handleTransaction}
+				loading={isLoading}
+				disabled={!amount}>
+				Transfer
+			</Button>
+		</Stack>
+	)
+}
+
+export default function Wallet({ user }: Props) {
+	const theme = useMantineTheme()
+
+	const isAboveLg = useMediaQuery(`(min-width: ${theme.breakpoints.lg})`)
+
+	const { data: transfers } = useGetFidorInternalTransfersQuery()
+
+	const transactions: {
+		id: string
+		date: Date
+		app: Transaction | null
+		fidor: FidorInternalTransfer | null
+	}[] = useMemo(
+		() =>
+			[
+				...user.app.transactions.map(t => ({
+					id: t.id,
+					date: new Date(t.created_at),
+					app: t,
+					fidor: transfers?.data.find(tf => tf.external_uid === t.id) ?? null
+				})),
+				...(transfers?.data ?? [])
+					.filter(t => !user.app.transactions.find(tx => tx.id === t.external_uid))
+					.map(t => ({
+						id: t.id!,
+						date: new Date(t.created_at!),
+						app: null,
+						fidor: t
+					}))
+			].sort((a, b) => b.date.getTime() - a.date.getTime()),
+		[user, transfers]
+	)
+	const [opened, { open, close }] = useDisclosure(false)
+
 	const getCurrency = (t: (typeof transactions)[number], tt: TransactionType): CURRENCY =>
 		(t.fidor?.currency as CURRENCY | undefined) ||
 		(t.app?.currency_pair
@@ -110,25 +153,45 @@ export default function Wallet({ user }: Props) {
 			</Head>
 
 			<Flex>
-				<Stack sx={{ flex: 1 }}>
-					<Title mt="md">Wallet</Title>
+				<Stack
+					sx={{ flex: 1 }}
+					w="100%">
+					<Flex
+						mt="md"
+						justify="space-between"
+						align="center">
+						<Title w="fit-content">Wallet</Title>
+						{!isAboveLg && (
+							<ActionIcon
+								size="lg"
+								variant="light"
+								onClick={open}>
+								<IconShoppingCart />
+							</ActionIcon>
+						)}
+					</Flex>
 
 					<Grid
-						columns={21}
-						gutter={12}>
+						columns={42}
+						gutter={isAboveLg ? 12 : 6}>
 						{CURRENCIES.map(c => (
 							<Grid.Col
 								key={c}
-								span={3}>
+								sm={14}
+								md={6}
+								span={21}>
 								<Card
 									withBorder
 									p="xs">
-									<Text align="center">
+									<Text
+										align="center"
+										truncate>
 										{CURRENCY_FLAGS[c]} {c}{" "}
 									</Text>
 									<Text
 										align="center"
-										weight={700}>
+										weight={700}
+										truncate>
 										{(user.app.balances[c] ?? 0).toFixed(5)}
 									</Text>
 								</Card>
@@ -136,106 +199,93 @@ export default function Wallet({ user }: Props) {
 						))}
 					</Grid>
 
-					<Table
-						sx={{
-							"& th:not(:first-of-type), & td:not(:first-of-type)": {
-								textAlign: "center !important" as "center"
-							},
-							borderRadius: 5
-						}}
-						bg={theme.colors.dark[6]}
-						withBorder
-						withColumnBorders>
-						<thead>
-							<tr>
-								<th>Transaction Date</th>
-								<th>Type</th>
-								<th>Paid</th>
-								<th>Received</th>
-								<th>Currency Pair</th>
-								<th>Price</th>
-							</tr>
-						</thead>
-						<tbody>
-							<AnimatePresence>
-								{transactions.map(t => (
-									<motion.tr
-										key={t.id}
-										layoutId={t.id}
-										style={{ background: theme.colors.dark[6] }}>
-										<td>
-											{t.date.toLocaleString("en-SG", {
-												year: "numeric",
-												month: "long",
-												day: "numeric",
-												hour: "numeric",
-												minute: "numeric",
-												second: "numeric"
-											})}
-										</td>
-										<td>
-											{t.app?.currency_pair === null ? (
-												<Badge color="green">Deposit</Badge>
-											) : t.app ? (
-												<Badge color="red">Exchange</Badge>
-											) : (
-												<Badge color="blue">Fidor</Badge>
-											)}
-										</td>
-										<td>
-											{(t.app?.amount ?? t.fidor?.amount)?.toFixed(5)}
-											{" " + getCurrency(t, "buy")}
-										</td>
-										<td>
-											{(
-												(t.app?.amount ?? t.fidor?.amount ?? 0) *
-												(t.app?.price ?? 0)
-											).toFixed(5)}
-											{" " + getCurrency(t, "sell")}
-										</td>
-										<td>{t.app?.currency_pair?.replace("_", " / ") ?? "-"}</td>
-										<td>{t.app?.price.toFixed(5) ?? "-"}</td>
-									</motion.tr>
-								))}
-							</AnimatePresence>
-						</tbody>
-					</Table>
+					<Box sx={{ overflowX: "scroll", whiteSpace: "nowrap" }}>
+						<Table
+							sx={{
+								"& th:not(:first-of-type), & td:not(:first-of-type)": {
+									textAlign: "center !important" as "center"
+								},
+								borderRadius: 5
+							}}
+							bg={theme.colors.dark[6]}
+							withBorder
+							withColumnBorders>
+							<thead>
+								<tr>
+									<th>Transaction Date</th>
+									<th>Type</th>
+									<th>Paid</th>
+									<th>Received</th>
+									<th>Currency Pair</th>
+									<th>Price</th>
+								</tr>
+							</thead>
+							<tbody>
+								<AnimatePresence>
+									{transactions.map(t => (
+										<motion.tr
+											key={t.id}
+											layoutId={t.id}
+											style={{ background: theme.colors.dark[6] }}>
+											<td>
+												{t.date.toLocaleString("en-SG", {
+													year: "numeric",
+													month: "long",
+													day: "numeric",
+													hour: "numeric",
+													minute: "numeric",
+													second: "numeric"
+												})}
+											</td>
+											<td>
+												{t.app?.currency_pair === null ? (
+													<Badge color="green">Deposit</Badge>
+												) : t.app ? (
+													<Badge color="red">Exchange</Badge>
+												) : (
+													<Badge color="blue">Fidor</Badge>
+												)}
+											</td>
+											<td>
+												{(t.app?.amount ?? t.fidor?.amount)?.toFixed(5)}
+												{" " + getCurrency(t, "buy")}
+											</td>
+											<td>
+												{(
+													(t.app?.amount ?? t.fidor?.amount ?? 0) *
+													(t.app?.price ?? 0)
+												).toFixed(5)}
+												{" " + getCurrency(t, "sell")}
+											</td>
+											<td>
+												{t.app?.currency_pair?.replace("_", " / ") ?? "-"}
+											</td>
+											<td>{t.app?.price.toFixed(5) ?? "-"}</td>
+										</motion.tr>
+									))}
+								</AnimatePresence>
+							</tbody>
+						</Table>
+					</Box>
 				</Stack>
 
-				<Divider
-					orientation="vertical"
-					mx="md"
-				/>
+				{isAboveLg ? (
+					<>
+						<Divider
+							orientation="vertical"
+							mx="md"
+						/>
 
-				<Stack w={250}>
-					<Title
-						mt="md"
-						order={2}>
-						Fidor to Markex
-					</Title>
-
-					<Text
-						c="dimmed"
-						fz="xs">
-						Transfer SGD from your Fidor Bank Account to your Markex Wallet!
-					</Text>
-
-					<NumberInput
-						label="Amount"
-						description="Amount of SGD to transfer"
-						value={amount}
-						onChange={setAmount}
-						hideControls
-					/>
-
-					<Button
-						color="green"
-						onClick={handleTransaction}
-						loading={isLoading}
-						disabled={!amount}>
-						Transfer
-					</Button>
-				</Stack>
+						<FidorToMarkex />
+					</>
+				) : (
+					<Drawer
+						opened={opened}
+						onClose={close}
+						position="right">
+						<FidorToMarkex isDrawer />
+					</Drawer>
+				)}
 			</Flex>
 		</Shell>
 	)
