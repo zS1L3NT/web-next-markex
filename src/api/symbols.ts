ec98d94f0f14f6c22624257633baabbc3b09032e
rxtay <77395408+rxtay@users.noreply.github.com>
2023-09-13 15:49:02 +0800

    add get stock symbol query

--- src/api/symbols.ts
+++ src/api/symbols.ts
@@ -8,7 +8,12 @@ const symbols = api.injectEndpoints({
 	endpoints: builder => ({
 		getAlpacaSymbols: builder.query<AlpacaSymbol[], void>({
 			query: () => ({
-				url: `${TRADING_API_ENDPOINT}/v2/assets?status=active&class=us_equity&exchange=NASDAQ`,
+				url: `${TRADING_API_ENDPOINT}/v2/assets?` +
+				new URLSearchParams({
+					status: "active",
+					class: "us_equity",
+					exchange: "NASDAQ"
+				}),
 				method: "GET",
 				headers: {
 					"Apca-Api-Key-Id": `${process.env.NEXT_PUBLIC_APCA_API_KEY_ID}`,
@@ -17,6 +22,17 @@ const symbols = api.injectEndpoints({
 			}),
 			transformResponse: ensureResponseType(arrayOf(AlpacaSymbol)),
 		}),
+		getAlpacaSymbol: builder.query<AlpacaSymbol, { symbol : string }>({
+			query: ({ symbol }) => ({
+				url: `${TRADING_API_ENDPOINT}/v2/assets/${symbol}`,
+				method: "GET",
+				headers: {
+					"Apca-Api-Key-Id": `${process.env.NEXT_PUBLIC_APCA_API_KEY_ID}`,
+					"Apca-Api-Secret-Key": `${process.env.NEXT_PUBLIC_APCA_API_SECRET_KEY}`
+				},
+			}),
+			transformResponse: ensureResponseType(AlpacaSymbol),
+		})
 	}),
 })
 
