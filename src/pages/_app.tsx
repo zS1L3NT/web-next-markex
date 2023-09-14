6147897c0cab479d13f8644c761e675e7f1e8158
rxtay <77395408+rxtay@users.noreply.github.com>
2023-09-14 18:37:24 +0800

    add new stock live price context; receive live quotes and trades of a stock symbol

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,23 +5,26 @@ import { MantineProvider } from "@mantine/core"
 import { Notifications } from "@mantine/notifications"
 
 import { CurrencyPairPricesProvider } from "@/contexts/CurrencyPairPricesContext"
+import { StockLivePricesProvider } from "@/contexts/StockLivePricesContext"
 import { NavigatorProvider } from "@/contexts/NavigatorContext"
 import store from "@/store"
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ReduxProvider store={store}>
-			<CurrencyPairPricesProvider>
-				<NavigatorProvider>
-					<MantineProvider
-						withGlobalStyles
-						withNormalizeCSS
-						theme={{ colorScheme: "dark" }}>
-						<Notifications />
-						<Component {...pageProps} />
-					</MantineProvider>
-				</NavigatorProvider>
-			</CurrencyPairPricesProvider>
+			<StockLivePricesProvider>
+				<CurrencyPairPricesProvider>
+					<NavigatorProvider>
+						<MantineProvider
+							withGlobalStyles
+							withNormalizeCSS
+							theme={{ colorScheme: "dark" }}>
+							<Notifications />
+							<Component {...pageProps} />
+						</MantineProvider>
+					</NavigatorProvider>
+				</CurrencyPairPricesProvider>
+			</StockLivePricesProvider>
 		</ReduxProvider>
 	)
 }
