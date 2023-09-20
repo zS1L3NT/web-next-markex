1e9d1cba982bf39e17f08a6164f91d39457a60b4
zS1L3NT Mac <zechariahtan144@gmail.com>
2023-09-20 12:51:21 +0800

    ✨ add seo and register service worker

--- next.config.js
+++ next.config.js
@@ -11,4 +11,7 @@ const nextConfig = {
 	}
 }
 
-module.exports = nextConfig
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa")({ dest: "public" })
+
+module.exports = withPWA(nextConfig)
